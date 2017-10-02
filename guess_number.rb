eec7600656ebb9513785f380c58003bd74d27fc0
Mike Dacanay <migd64@gmail.com>
2017-10-02 16:20:42 -0700

    final commit

--- guess_number.rb
+++ guess_number.rb
@@ -1,5 +1,8 @@
-def guess_number name1
-	puts "hello, #{name1}"
+def guess_number guess
+	number = 25
+	puts "You got it" unless guess != 25
+	puts "Guess was too high!" unless guess < 25
+	puts "Guess was too low!" unless guess > 25
 end
 
-guess_number "Mike"
+guess_number 26
